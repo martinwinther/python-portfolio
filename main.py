58ed6555eee747732f5fd34f68d05e61c506c4dc
Martin Winther <martin.winther@protonmail.com>
2023-09-29 12:39:29 +0200

    Day 21 Added basic layout and images

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Martin Winther")
+    bio = """
+    Hi, I'm Martin. A software engineer based in Aarhus, Denmark. I'm passionate about building cool stuff.
+    I'm currently working on a project to build a web-based dashboard to monitor the performance of my company's
+    production systems. I'm also interested in learning new technologies and exploring new ways to build software.
+    Feel free to reach out to me if you have any questions or want to collaborate.
+    
+    """
+    st.info(bio)
+
+content = """
+Below you can find a list of my projects that I have built in Python
+"""
+st.write(content)
